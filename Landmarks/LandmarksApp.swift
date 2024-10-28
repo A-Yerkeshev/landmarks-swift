5cb1bd70d3e1dc1089c2cd68ffaf458f75d86f6f
Arman Yerkesh <a.yerkesh@gmail.com>
2024-10-28 11:03:58 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Arman Yerkeshev on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
